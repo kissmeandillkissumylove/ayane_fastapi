44026437b89da8c2a8cf018489c43cfef48fe2e0
kissmeandillkissumylove <kissmeandillkissumylove@gmail.com>
2023-10-19 13:19:54 +0700

    load variables from .env

---
+++ config.py
@@ -0,0 +1,13 @@
+"""config file for getting variables from .env.
+10.11.2023 https://github.com/kissmeandillkissumylove"""
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
